6059c35d9bf99a079015e4b174d9dba37b7d7e9c
isaque <isaquefernando10@gmail.com>
2023-01-17 11:21:49 -0300

    feat(conteudo): adicionando novas funções para lidar com arquivo

--- contrib/validators.py
+++ contrib/validators.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def validate_document_format(file):
     extension = Path(file.file.name).suffix
-    if not extension in ['.doc', '.pdf', '.zip']:
+    if extension not in ['.doc', '.pdf', '.zip']:
         raise ValidationError(
             _('Formato inválido para o arquivo. Os formatos válidos são .doc, .pdf or .zip')
         )
@@ -13,7 +13,7 @@ def validate_document_format(file):
 
 def validate_image_format(file):
     extension = Path(file.file.name).suffix
-    if not extension in ['.png', '.jpg', '.jpeg']:
+    if extension not in ['.png', '.jpg', '.jpeg']:
         raise ValidationError(
             _('Formato inválido para o arquivo. Os formatos válidos são .png, .jpg or .jpeg')
         )
@@ -31,7 +31,7 @@ def validate_file_max_size(file, max_size_kb: int):
     file_size = file.file.size
     limit_kb = max_size_kb
     if file_size > limit_kb * 1024:
-        raise ValidationError("Tamanho máximo do arquivo é de %s KB" % limit_kb)
+        raise ValidationError(f"Tamanho máximo do arquivo é de {limit_kb} KB")
 
 
 def validate_image_max_300(image):
@@ -76,3 +76,25 @@ def validate_file_max_200000(file):
 
 def validate_file_max_300000(file):
     return validate_file_max_size(file, 300000)
+
+
+def validate_string_limit(value: str, max_length: int):
+    if len(value) > max_length:
+        raise ValidationError(_('O campo possui mais caracteres que o permitido.'
+                                ' O limite atual é de {} caracteres.').format(max_length))
+
+
+def validate_string_limit_20(value):
+    return validate_string_limit(value, 20)
+
+
+def validate_string_limit_100(value: str):
+    return validate_string_limit(value, 100)
+
+
+def validate_upc_record(value: str) -> None:
+    import re
+    if re.match(r"^((\d{14})?|(\d{14})(\/\d{14})*)$", value) is None:
+        raise ValidationError(_('O campo UPC deve conter exatamente 14 números separados apenas por / quando mais de'
+                                ' um registro precisa ser inserido.'
+                                ' Ex: 123456789012345 ou 123456789012345/123456789012345.'))
